983294f5b3fc833e61ab53512b0c0365fd933c8f
DedB0r0ded <revikad.00@mail.ru>
2024-06-09 13:04:34 +0300

    v 0.0.1 build win_alpha0906241304 Implemented more REST structures.

--- src/REST_Classes.cpp
+++ src/REST_Classes.cpp
@@ -45,3 +45,66 @@ bool RegKey::operator!=(const RegKey &other) const
 {
   return !(*this == other);
 }
+
+bool Contract::operator==(const Contract &other) const
+{
+  return  id == other.id
+          && continuous == other.continuous
+          && urgent == other.urgent
+          && subject == other.subject
+          && total_cost == other.total_cost
+          && concludedOn == other.concludedOn
+          && expiresOn == other.expiresOn
+          && contractor == other.contractor
+          && customer == other.customer;
+}
+
+bool Contract::operator!=(const Contract &other) const
+{
+  return !(*this == other);
+}
+
+bool Manufacturer::operator==(const Manufacturer &other) const
+{
+  return  id == other.id
+          && rating == other.rating
+          && code == other.code
+          && name == other.name
+          && country == other.country
+          && description == other.description;
+}
+
+bool Manufacturer::operator!=(const Manufacturer &other) const
+{
+  return !(*this == other);
+}
+
+bool Component::operator==(const Component &other) const
+{
+  return  id == other.id
+          && manufactureYear == other.manufactureYear
+          && name == other.name
+          && serialNumber == other.serialNumber
+          && manufacturer == other.manufacturer
+          && description == other.description;
+}
+
+bool Component::operator!=(const Component &other) const
+{
+  return !(*this == other);
+}
+
+bool Facility::operator==(const Facility &other) const
+{
+  return  id == other.id
+          && manufactureYear == other.manufactureYear
+          && name == other.name
+          && serialNumber == other.serialNumber
+          && manufacturer == other.manufacturer
+          && description == other.description;
+}
+
+bool Facility::operator!=(const Facility &other) const
+{
+  return !(*this == other);
+}
--- src/REST_Classes.h
+++ src/REST_Classes.h
@@ -2,6 +2,7 @@
 #define REST_CLASSES_H
 
 #include <QObject>
+#include <QDate>
 
 struct User{
   size_t id;
@@ -37,6 +38,59 @@ struct RegKey{
   bool admin;
   bool operator==(const RegKey& other) const;
   bool operator!=(const RegKey& other) const;
+};
+
+struct Contract{
+  size_t id;
+  bool continuous;
+  bool urgent;
+  QString subject;
+  QString pdf_path;
+  int total_cost;
+  QDate concludedOn;
+  QDate expiresOn;
+  Organisation contractor;
+  Organisation customer;
+
+  bool operator==(const Contract& other) const;
+  bool operator!=(const Contract& other) const;
+};
+
+struct Manufacturer{
+  size_t id;
+  QString name;
+  QString code;
+  QString country;
+  QString description;
+  int rating;
+  bool operator==(const Manufacturer& other) const;
+  bool operator!=(const Manufacturer& other) const;
+};
+
+struct Component{
+  size_t id;
+  QString name;
+  QString description;
+  QString serialNumber;
+  QDate manufactureYear;
+  QString pictureRoute;
+  Manufacturer manufacturer;
+
+  bool operator==(const Component& other) const;
+  bool operator!=(const Component& other) const;
+};
+
+struct Facility{
+  size_t id;
+  QString name;
+  QString description;
+  QString serialNumber;
+  QDate manufactureYear;
+  QString pictureRoute;
+  Manufacturer manufacturer;
+
+  bool operator==(const Facility& other) const;
+  bool operator!=(const Facility& other) const;
 };
 
 #endif
